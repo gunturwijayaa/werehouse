0bd569382e2e1dfd49fb6e79fb99803f8a248134
guntur <gunturwijayah@gmail.com>
2024-04-02 14:31:57 +0700

    fitur profile : perubahan dan penambahan pada button akun, edit profile, keamanan , detail pribadi.

--- lib/dashboard/homepage/profile.dart
+++ lib/dashboard/homepage/profile.dart
@@ -28,6 +28,29 @@ class _RootAppState extends State<RootApp> {
 
   @override
   Widget build(BuildContext context) {
+    List<CustomListTile> customListTiles = [
+      CustomListTile(
+        icon: Icons.person,
+        title: "akun",
+      ),
+      CustomListTile(
+        icon: Icons.edit,
+        title: "Edit Profile",
+      ),
+      CustomListTile(
+        icon: Icons.lock,
+        title: "Kata Sandi dan Keamanan",
+      ),
+      CustomListTile(
+        title: "Detail Pribadi",
+        icon: CupertinoIcons.person_2_alt,
+      ),
+      CustomListTile(
+        title: "Logout",
+        icon: CupertinoIcons.arrow_right_arrow_left,
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: true,
@@ -45,59 +68,63 @@ class _RootAppState extends State<RootApp> {
             children: [
               Stack(
                 children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.white,
-                    backgroundImage: _image != null
-                        ? FileImage(_image!) as ImageProvider<Object>
-                        : const NetworkImage(
-                            "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Images.png",
-                          ),
+                  GestureDetector(
+                    onTap: _getImage,
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      backgroundImage: _image != null
+                          ? FileImage(_image!) as ImageProvider<Object>
+                          : const NetworkImage(
+                              "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Images.png",
+                            ),
+                    ),
                   ),
                   Positioned(
-  bottom: 5,
-  right: -2,
-  child: Stack(
-    children: [
-      Container(
-        width: 29, // menyesuaikan dengan lebar icon dan outline
-        height: 29, // menyesuaikan dengan tinggi icon dan outline
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.blue,
-        ),
-      ),
-      Positioned(
-        top: 0,
-        left: 0,
-        child: Container(
-          width: 29,
-          height: 29,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(
-              color: Colors.white,
-              width: 2,
-            ),
-          ),
-        ),
-      ),
-      Positioned(
-        child: IconButton(
-          onPressed: _getImage,
-          icon: Icon(
-            Icons.add,
-            size: 17,
-            color: Colors.white,
-          ),
-        ),
-        left: -9, // menyesuaikan posisi ikon dalam kotak biru
-        top: -9, // menyesuaikan posisi ikon dalam kotak biru
-      ),
-    ],
-  ),
-),
-
+                    bottom: 5,
+                    right: -2,
+                    child: Stack(
+                      children: [
+                        Container(
+                          width:
+                              29, // menyesuaikan dengan lebar icon dan outline
+                          height:
+                              29, // menyesuaikan dengan tinggi icon dan outline
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          child: Container(
+                            width: 29,
+                            height: 29,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          child: IconButton(
+                            onPressed: _getImage,
+                            icon: Icon(
+                              Icons.add,
+                              size: 17,
+                              color: Colors.white,
+                            ),
+                          ),
+                          left: -9, // menyesuaikan posisi ikon dalam kotak biru
+                          top: -9, // menyesuaikan posisi ikon dalam kotak biru
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
               SizedBox(height: 10),
@@ -108,98 +135,31 @@ class _RootAppState extends State<RootApp> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Text("~ User ~")
-            ],
-          ),
-          const SizedBox(height: 25),
-          Row(
-            children: const [
-              Padding(
-                padding: EdgeInsets.only(right: 5),
-                child: Text(
-                  "Complete your profile",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+              Text("~ User ~"),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  // Tambahkan fungsi onPressed sesuai kebutuhan
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // Warna latar belakang tombol
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30), // Mengatur border radius
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text(
+                    "Edit", // Teks pada tombol
+                    style: TextStyle(
+                      color: Colors.white, // Warna teks
+                      fontSize: 16, // Ukuran teks
+                    ),
                   ),
                 ),
               ),
-              Text(
-                "(1/5)",
-                style: TextStyle(
-                  color: Colors.blue,
-                ),
-              )
             ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: List.generate(5, (index) {
-              return Expanded(
-                child: Container(
-                  height: 7,
-                  margin: EdgeInsets.only(right: index == 4 ? 0 : 6),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: index == 0 ? Colors.blue : Colors.black12,
-                  ),
-                ),
-              );
-            }),
-          ),
-          const SizedBox(height: 10),
-          SizedBox(
-            height: 180,
-            child: ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                final card = profileCompletionCards[index];
-                return SizedBox(
-                  width: 160,
-                  child: Card(
-                    shadowColor: Colors.black12,
-                    child: Padding(
-                      padding: const EdgeInsets.all(15),
-                      child: Column(
-                        children: [
-                          Icon(
-                            card.icon,
-                            size: 30,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            card.title,
-                            textAlign: TextAlign.center,
-                          ),
-                          const Spacer(),
-                          ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              elevation: 0,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              backgroundColor: Colors
-                                  .blue, // Memberikan warna biru pada tombol
-                            ),
-                            child: Text(
-                              card.buttonText,
-                              style: TextStyle(
-                                  color: Colors
-                                      .white), // Memberikan warna teks putih
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) =>
-                  const Padding(padding: EdgeInsets.only(right: 5)),
-              itemCount: profileCompletionCards.length,
-            ),
-          ),
+          ),         
           const SizedBox(height: 20),
           ...List.generate(
             customListTiles.length,
@@ -210,6 +170,9 @@ class _RootAppState extends State<RootApp> {
                 child: Card(
                   elevation: 4,
                   shadowColor: Colors.black12,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30), // Mengatur border radius
+                  ),
                   child: ListTile(
                     leading: Icon(tile.icon),
                     title: Text(
@@ -231,36 +194,6 @@ class _RootAppState extends State<RootApp> {
   }
 }
 
-class ProfileCompletionCard {
-  final String title;
-  final String buttonText;
-  final IconData icon;
-
-  ProfileCompletionCard({
-    required this.title,
-    required this.buttonText,
-    required this.icon,
-  });
-}
-
-List<ProfileCompletionCard> profileCompletionCards = [
-  ProfileCompletionCard(
-    title: "Set Your Profile Details",
-    icon: CupertinoIcons.person_circle,
-    buttonText: "Continue",
-  ),
-  ProfileCompletionCard(
-    title: "Upload your resume",
-    icon: CupertinoIcons.doc,
-    buttonText: "Upload",
-  ),
-  ProfileCompletionCard(
-    title: "Add your skills",
-    icon: CupertinoIcons.square_list,
-    buttonText: "Add",
-  ),
-];
-
 class CustomListTile {
   final IconData icon;
   final String title;
@@ -269,22 +202,3 @@ class CustomListTile {
     required this.title,
   });
 }
-
-List<CustomListTile> customListTiles = [
-  CustomListTile(
-    icon: Icons.insights,
-    title: "Activity",
-  ),
-  CustomListTile(
-    icon: Icons.location_on_outlined,
-    title: "Location",
-  ),
-  CustomListTile(
-    title: "Notifications",
-    icon: CupertinoIcons.bell,
-  ),
-  CustomListTile(
-    title: "Logout",
-    icon: CupertinoIcons.arrow_right_arrow_left,
-  ),
-];
